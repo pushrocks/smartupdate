7bfdb8db5e2bccdd25d2f3adbc3497e17956a670
Phil Kunz <github@philkunz.com>
2018-09-02 12:41:06 +0200

    fix(dependencies): update to latest versions

--- ts/smartupdate.plugins.ts
+++ ts/smartupdate.plugins.ts
@@ -5,6 +5,6 @@ import * as npmextra from '@pushrocks/npmextra';
 import * as smartnpm from '@pushrocks/smartnpm';
 import * as smartopen from '@pushrocks/smartopen';
 import * as smarttime from '@pushrocks/smarttime';
-import * as smartversion from 'smartversion';
+import * as smartversion from '@pushrocks/smartversion';
 
 export { smartlog, consolecolor, npmextra, smartnpm, smartopen, smarttime, smartversion };
